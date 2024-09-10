4cb4cae397a1efbc3f1fe1b5f467fb3ab7e81a6d
Julien Marrec <julien.marrec@gmail.com>
2024-09-10 20:43:36 +0200

    Add python 3.12.2 and ruby 3.2.2
    Had to patch the repo list for centos7 since as of July 2024, the default image ones is no longer working. This images is more than EOL, it's just legacy.

--- docker_container_script.sh
+++ docker_container_script.sh
@@ -2,39 +2,55 @@
 
 set -e -x
 
-cd OS-build-release
-
 # This isn't needed if you pass --login to bash or if you are already attached to the container
 # source scl_source enable devtoolset-10
-
-# TODO: temp: I'm trying to avoid redownloading everything all the time
-#cp ../dropbox/EnergyPlus-22.1.0-ed759b17ee-Centos7-x86_64.tar.gz .
-#tar xfz EnergyPlus-22.1.0-ed759b17ee-Centos7-x86_64.tar.gz
-#cp ../dropbox/openstudio3-gems-20220404-linux.tar.gz .
-#tar xfz openstudio3-gems-20220404-linux.tar.gz
-#cp ../dropbox/radiance-5.0.a.12-Redhat.tar.gz .
-#tar xfz radiance-5.0.a.12-Redhat.tar.gz
-
 gcc --version
 g++ --version
 
-# Initially I set that to ON, then I did
-# conan remote add openstudio-centos https://conan.openstudio.net/artifactory/api/conan/openstudio-centos
-# conan user -p <TOKEN> -r openstudio-centos jmarrec
-# conan upload -r openstudio-centos --all --parallel --no-overwrite all --confirm "*"
-CONAN_FIRST_TIME_BUILD_ALL=OFF
+# TODO: temp: I'm trying to avoid redownloading everything all the time
+#cp ../dropbox/EnergyPlus-23.1.0-87ed9199d4-Linux-CentOS7.9.2009-x86_64.tar.gz .
+#tar xfz EnergyPlus-23.1.0-87ed9199d4-Linux-CentOS7.9.2009-x86_64.tar.gz
+#cp ../dropbox/openstudio3-gems-20230427-linux.tar.gz .
+#tar xfz openstudio3-gems-20230427-linux.tar.gz
+#cp ../dropbox/radiance-5.0.a.12-Redhat.tar.gz .
+#tar xfz radiance-5.0.a.12-Redhat.tar.gz
+mkdir EnergyPlus-build-release
+cd EnergyPlus-build-release
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DLINK_WITH_PYTHON:BOOL=ON -DBUILD_TESTING:BOOL=ON \
+  -DBUILD_FORTRAN:BOOL=ON -DDOCUMENTATION_BUILD:STRING=DoNotBuild \
+  -DENABLE_GTEST_DEBUG_MODE=OFF \
+  -DBUILD_PACKAGE:BOOL=ON -DCPACK_BINARY_IFW:BOOL=OFF -DCPACK_BINARY_STGZ:BOOL=OFF -DCPACK_BINARY_TGZ:BOOL=ON \
+  -DPython_REQUIRED_VERSION:STRING=3.12.2 \
+  -DPython_ROOT_DIR:PATH=$HOME/.pyenv/versions/3.12.2 \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON \
+  ../EnergyPlus
+ninja package
+
+cd ..
+
+cd OpenStudio
+
+CONAN_FIRST_TIME_BUILD_ALL=true
 echo "CONAN_FIRST_TIME_BUILD_ALL=$CONAN_FIRST_TIME_BUILD_ALL"
 
-cmake -G Ninja  -DCMAKE_BUILD_TYPE:STRING=Release \
-      -DBUILD_TESTING:BOOL=ON -DCPACK_BINARY_TGZ:BOOL=ON -DCPACK_BINARY_RPM:BOOL=ON \
-      -DCPACK_BINARY_IFW:BOOL=OFF -DCPACK_BINARY_DEB:BOOL=OFF -DCPACK_BINARY_NSIS:BOOL=OFF \
-      -DCPACK_BINARY_STGZ:BOOL=OFF -DCPACK_BINARY_TBZ2:BOOL=OFF -DCPACK_BINARY_TXZ:BOOL=OFF -DCPACK_BINARY_TZ:BOOL=OFF \
-      -DBUILD_PYTHON_BINDINGS:BOOL=ON -DPYTHON_VERSION:STRING=3.8 \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON \
-      -DCMAKE_C_COMPILER:FILEPATH=$CC -DCMAKE_CXX_COMPILER:FILEPATH=$CXX \
-      -DCONAN_FIRST_TIME_BUILD_ALL:BOOL=$CONAN_FIRST_TIME_BUILD_ALL -D_GLIBCXX_USE_CXX11_ABI=0 \
-      -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON \
-      ../OpenStudio
+if [[ "$CONAN_FIRST_TIME_BUILD_ALL" == true ]]; then
+  conan install . --output-folder=../OS-build-release --build='*' -c tools.cmake.cmaketoolchain:generator=Ninja -s compiler.cppstd=20 -s build_type=Release
+else
+  conan install . --output-folder=../OS-build-release --build=missing -c tools.cmake.cmaketoolchain:generator=Ninja -s compiler.cppstd=20 -s build_type=Release
+fi
+CONAN_FIRST_TIME_BUILD_ALL=false
+
+cmake --preset conan-release -DPYTHON_VERSION=3.12.2 -DPython_ROOT_DIR:PATH=$HOME/.pyenv/versions/3.12.2
+
+# Initially I set that to ON, then I did
+# conan remote add -i 0 openstudio-centos https://conan.openstudio.net/artifactory/api/conan/openstudio-centos
+# conan user -p <TOKEN> -r openstudio-centos jmarrec
+# conan upload -r openstudio-centos --all --parallel --no-overwrite all --confirm "*"
+
+
+
 
 echo "Changing shebang line in radiance"
 sed -i "s:#\!/usr/local/bin/wish4.0:#\!/usr/bin/env wish:g" radiance-5.0.a.12-Linux/usr/local/radiance/bin/trad
